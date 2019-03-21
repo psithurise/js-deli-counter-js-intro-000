093fbd7154c5671a18781343f74f881ab64efa98
Rose Stough <psithurise@programmer.net>
2019-03-21 20:54:42 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,15 @@
+function takeANumber(line, name) {
+  //console.log "Welcome, [name]. You are number [line] in line."
+}
+
+function nowServing(line) {
+  //return 1st person in line with "Currently serving [name]", remove person from current line
+  //if noone in line, return "There is nobody waiting to be served!"
+}
+
+function currentLine(line) {
+  //returns line as string. if array is ["Ada", "Grace"]
+  // currentLine(katzDeliLine) would return
+  //"The line is currently: 1. Ada, 2. Grace"
+  //if noone, "The line is currently empty"
+}
